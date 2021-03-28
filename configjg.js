1bb21153df1d0d5188aa1934e116f17e917087ae
Wai13 <62482039+Wai13@users.noreply.github.com>
2021-03-28 15:33:39 +0300

    123123
    123123

---
+++ configjg.js
@@ -0,0 +1,48 @@
+
+// Просто файл настройки бота, где можно указать основные параметры. Особо вдаваться в подробности, я не собираюсь
+
+
+module.exports = {
+
+  // Имя бота
+
+  botName:        `WorldPlay`,
+
+  // Параметры группы
+
+    groupToken: '10aa50a364fb1da10226e86d56305e4141a3f50f45d7bac6374521ff57c6a68cf93515f4025ed01995514',
+    groupID: 192580900,
+
+  // VKCoin
+
+    vkcKey: `8wUPtR6zzvE5hmB0.cV;upRzovdW-ZHj8c_TjwK;9d2SJ-nti3`,
+    vkcID: `416528201`,
+  
+  // PaperScroll
+    
+    psid: `2342344`,
+    pskey: `234324234`,
+
+  // CoronaCoin
+
+    cckey: `2342342342343`,
+    ccid: ``,
+
+  // GameCoin
+    gckey: `234234234234`,
+    gcid: ``,
+
+
+  // База данных
+
+  urlDB:          'mongodb+srv://Waii:artemadmin@cluster0.hws9t.mongodb.net/just?retryWrites=true&w=majority',
+  nameDB:         'just',
+
+  // Награды за топ
+
+    PSTop: [2000000, 1000000, 500000, 250000, 150000, 100000, 50000, 50000, 50000, 50000],
+
+
+
+
+};
